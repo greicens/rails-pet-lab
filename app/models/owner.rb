c45d203c280baca5aea8d14160b780f075b41e9d
greicens <gsilva@mail.ccsf.edu>
2017-01-25 13:38:01 -0800

    create a one to many relatioship between owners and pets

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,11 +1,10 @@
 class Owner < ActiveRecord::Base
-  # TODO: add association ot pets
-  
   # TODO: add association to appointments (through pets)
-  
+
   # TODO: add validations
 
   before_save :normalize_phone_number
+  has_many :pets
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,12 +1,11 @@
 class Pet < ActiveRecord::Base
-  # TODO: associate with owner
-  
   # TODO: associate with appointments
 
   # TODO: validate name and breed
 
   validate :date_of_birth_cannot_be_in_the_future
 
+  belongs_to :owner , optional: true
   # adds an error if birth date is in the future
   def date_of_birth_cannot_be_in_the_future
     # stretch
